7e4a2526036764720f46a9b439fd021d6c374928
gbz3 <gebizo3@gmail.com>
2013-06-08 15:13:35 +0900

    2nd

--- src/com/github/gbz3/jnetcat/Main.java
+++ src/com/github/gbz3/jnetcat/Main.java
@@ -3,11 +3,16 @@
  */
 package com.github.gbz3.jnetcat;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 /**
  * @author gbz3
  *
  */
 public class Main {
+	private static final Logger log = LoggerFactory.getLogger( Main.class );
 
 	/**
 	 * @param args
