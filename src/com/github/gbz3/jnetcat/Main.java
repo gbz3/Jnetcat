5f482736cb7f5fb0c335546eb53283de419802a6
gbz3 <gebizo3@gmail.com>
2013-06-08 15:10:07 +0900

    1st

---
+++ src/com/github/gbz3/jnetcat/Main.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package com.github.gbz3.jnetcat;
+
+/**
+ * @author gbz3
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
